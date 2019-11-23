a3dfd97df4e8bc5fe7b59b79dff6b39f5b957237
Nicholas Hazel <nicholas.hazel@gmail.com>
2019-11-23 12:00:19 -0800

    Got all tests passing. Want confirmation this is what was intended?

--- src/components/card/Card.test.js
+++ src/components/card/Card.test.js
@@ -5,10 +5,11 @@ import renderer from 'react-test-renderer';
 import STORE from '../../STORE.js';
 
 describe('Card component', () => {
+	const store = {STORE};
+	const props = store.STORE.allCards['a'];
+
 	it('Renders without crashing', () => {
-		const store = {STORE};
 		const div = document.createElement('div');
-		const props = store.STORE.allCards['a'];
 		ReactDOM.render(
 			<Card
 				id={props.id}
@@ -17,4 +18,16 @@ describe('Card component', () => {
     	/>, div);
 		ReactDOM.unmountComponentAtNode(div);
 	});
+
+	it('renders the UI as expected', () => {
+		const tree = renderer
+			.create(<Card
+				id={props.id}
+				title={props.title}
+				content={props.content}
+    	/>)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
 })
--- src/components/list/List.test.js
+++ src/components/list/List.test.js
@@ -5,16 +5,28 @@ import renderer from 'react-test-renderer';
 import STORE from '../../STORE.js';
 
 describe('List component', () => {
+	const store = {STORE};
+	const props = store.STORE.lists[0];
+
 	it('Renders without crashing', () => {
-		const store = {STORE};
 		const div = document.createElement('div');
-		const props = store.STORE.lists[0];
 		ReactDOM.render(
 			<List 
-				id={"list-" + props.id}
+				key={"list-" + props.id}
       	title={props.header}
       	cards={props.cardIds.map(card => store.STORE.allCards[card])}
     	/>, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it('renders the UI as expected', () => {
+		const tree = renderer
+			.create(<List 
+				key={"list-" + props.id}
+      	title={props.header}
+      	cards={props.cardIds.map(card => store.STORE.allCards[card])}
+    	/>)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
 	});
 })
